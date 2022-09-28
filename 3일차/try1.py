89bb9849c0f8780873126d13e9660c8964769b24
kimsy <supernyoon@gmail.com>
2022-09-28 10:19:50 +0900

    예외처리연습2

--- 3일차/try1.py
+++ 3일차/try1.py
@@ -3,8 +3,18 @@
 def divide(a,b):
     return a/b
 
-#호출
-result = divide(5,0)
-print("결과 :{0}".format(result))
+#에레처리
+try:
+    #호출
+    # result = divide(5,0)    
+    result = divide(5,"ㅁㅁㅁ")    
+except ZeroDivisionError:
+    print("0으로 나누면 안됨")
+except TypeError:
+    print("숫자이어야 연산 가능")
+else:
+    print("결과 :{0}".format(result))
+finally:
+    print("무조건 실행")
 
 print("---전체 코드 종료---")
