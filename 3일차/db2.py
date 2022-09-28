50d9044d8a15a0faa3ef354fea9c7e342427e073
kimsy <supernyoon@gmail.com>
2022-09-28 16:51:42 +0900

    db연습3

--- 3일차/db2.py
+++ 3일차/db2.py
@@ -2,14 +2,16 @@
 import sqlite3
 
 #연결객체(d일단은 메모리에서 작업)
-con = sqlite3.connect(":memory:")
+#con = sqlite3.connect(":memory:")
+#실제 물리적인 파일에 저장
+con = sqlite3.connect("c:\\work\\3일차\\test.db")
+
 #구문을 실행하는 커서
 cur = con.cursor()
 #데이터 구조(Table)
 cur.execute("create table PhoneBook (Name text, PhoneNum text);") #빈칸 유의
 #1건 입력
 cur.execute("insert into PhoneBook values ('derick', '010-1111-1111');") #단일 따옴표 사용 유의 ' '
-
 #입력 파라미터 처리 : 보통은 아래처럼 변수로 받아 처리
 name = "gildong"
 phoneNumber = "010-4567-1234"
@@ -20,15 +22,7 @@ cur.executemany("insert into PhoneBook values (?,?);", datalist)
 
 #검색
 cur.execute("select * from PhoneBook;") #튜플로 리턴
-# for row in cur:
-#     print(row)
+print(cur.fetchall())
 
-print("----fetchone()---")
-print(cur.fetchone())
-print("----fetchmany(2)---")
-print(cur.fetchmany(2))
-print("----fetchall()---")
-print(cur.fetchall())
-cur.execute("select * from PhoneBook;") #튜플로 리턴
-print("----fetchall()---")
-print(cur.fetchall())
+#연결객체에서 완료
+con.commit()
