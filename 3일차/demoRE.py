0703365846c335e92d785b3fda743799e88984d9
kimsy <supernyoon@gmail.com>
2022-09-28 13:53:35 +0900

    로그 파일 분석

--- 3일차/demoRE.py
+++ 3일차/demoRE.py
@@ -9,3 +9,11 @@ print(result.group())
 # result = re.match("[0-9]*th", " 35th")
 # print(result)
 # print(result.group())
+
+result = re.search("apple", "this is apple")
+print(result.group())
+
+print("---년도찾기---")
+print(bool(re.search("\d{4}", "올해는 2022년도"))) #\d{4} digit 4자리
+result = re.search("\d{5}", "우리 동네는 53000") #숫자 5자리만 찾기
+print(result.group())
---
+++ 3일차/doc_copy_readline2.py
@@ -0,0 +1,28 @@
+import re 
+
+#원본 로그파일
+f=open('c:\\work\\3일차\\PV3.txt','rt')
+#복사본
+g=open('c:\\work\\3일차\\PV3_copy.txt','wt',encoding='utf-8')
+
+#보통 로그 파일은 용량이 큼 따라서
+#많은 라인의 파일이면 
+#한번에 한줄씩 읽어서 처리한다.  
+#파일의 EOF(End Of File)이 아니면 계속 읽도록 한다. 
+line = f.readline()
+while (line != ''):
+    #if (re.search("\d{4}", line)):  #숫자 4자리 찾아봐
+    if (re.search("error", line)):
+        g.write(line + "\n")
+    line = f.readline()
+
+f.close() 
+g.close()
+
+
+
+
+
+
+
+
