af3368c057af882ce36f109c5cd7a5a72fd47850
kimsy <supernyoon@gmail.com>
2022-09-29 13:18:16 +0900

    클리앙 중고장터 크롤링

--- 4일차/web1.py
+++ 4일차/web1.py
@@ -9,5 +9,13 @@ soup = BeautifulSoup(page, "html.parser")
 # print(soup.find_all("p"))
 #첫번째 p 검색
 # print(soup.find("p"))
+#조건을 주고 필터링
+# print(soup.find_all("p", class_="outer-text"))
+#특정 id 검색
+# print(soup.find_all(id="first"))
 
-print(soup.find_all("p", class_="outer-text"))
+#태그 내부에 컨텐츠만 ㄴ검색 (.text  속성 사용)
+for tag in soup.find_all("p"):
+    title = tag.text.strip() #앞뒤공백 없앰
+    title = title.replace("\n", "") #빈줄없앰
+    print(title)
